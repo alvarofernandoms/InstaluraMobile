1b28f2f2d41f1aefcab2e8c06577146ef4789512
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-01-23 10:21:24 -0200

    Add Post component

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, Dimensions, FlatList } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Post from './src/components/Post';
 
 const width = Dimensions.get('screen').width;
 
@@ -28,15 +16,7 @@ export default class App extends Component<{}> {
         data={fotos}
         keyExtractor={item => item.id}
         renderItem={({item}) =>
-          <View>
-            <View style={styles.cabecalho}>
-              <Image source={require('./resources/img/react-alura.jpg')}
-                style={styles.fotoDePerfil}/>
-              <Text>{item.usuario}</Text>
-            </View>
-            <Image source={require('./resources/img/react-alura.jpg')}
-              style={styles.foto}/>
-          </View>
+          <Post foto={item}/>
         }
       />
     );
@@ -46,20 +26,5 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     marginTop: 10
-  },
-  cabecalho: {
-    margin: 10,
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  fotoDePerfil: {
-    marginRight: 10,
-    borderRadius: 20,
-    width: 40,
-    height: 40
-  },
-  foto: {
-    width: width,
-    height: width
   }
 })
