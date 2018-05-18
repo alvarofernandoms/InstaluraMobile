61e9ba4a3246c9ea3a085922dd690a4871be046b
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-05-18 09:05:21 -0300

    Removing unnecessary import

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default class InputComentario extends Component {
 
